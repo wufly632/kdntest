bafd9fb23f22638d4657983b1ae9bc4940296d5c
wufly <fei.wu@patpat.com>
2018-10-25 12:00:01 +0800

    随机取sku

--- database/seeds/RequriementSeeder.php
+++ database/seeds/RequriementSeeder.php
@@ -11,13 +11,14 @@ class RequriementSeeder extends Seeder
      */
     public function run()
     {
+        $sku_ids = \App\Entities\Good\GoodSku::all()->pluck('id')->toArray();
         $data = [];
         for ($i = 0; $i < 100; $i++) {
             $tmp = [];
-            $tmp['sku_id'] = rand(1, 10);
+            $tmp['sku_id'] = $sku_ids[array_rand($sku_ids,1)];
             $tmp['num'] = rand(-20,30);
             $tmp['type'] = rand(1,2);
-            $tmp['is_push'] = rand(0,1);
+            $tmp['is_push'] = 0;
             $tmp['created_at'] = \Carbon\Carbon::now()->toDateTimeString();
             $data[] = $tmp;
         }
