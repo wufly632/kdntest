f3c8d14772718c58aa977d3f29b748743814c159
longyuan <longyuan@pdever.cn>
2018-10-29 20:45:08 +0800

    oss配置修改

--- bootstrap/aliyun-sts/aliyun-php-sdk-core/Regions/EndpointConfig.php
+++ bootstrap/aliyun-sts/aliyun-php-sdk-core/Regions/EndpointConfig.php
@@ -17,7 +17,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-$regionIds = array("cn-hangzhou","cn-beijing","cn-qingdao","cn-hongkong","cn-shanghai","us-west-1","cn-shenzhen","ap-southeast-1");
+$regionIds = array("cn-hangzhou","cn-beijing","cn-qingdao","cn-hongkong","cn-shanghai","us-west-1","cn-shenzhen","ap-southeast-1","ap-south-1");
 $productDomains =array(
 	new ProductDomain("Ecs", "ecs.aliyuncs.com"),
 	new ProductDomain("Rds", "rds.aliyuncs.com"),
@@ -25,7 +25,7 @@ $productDomains =array(
 	new ProductDomain("Bss", "bss.aliyuncs.com"),
 	new ProductDomain("Oms", "oms.aliyuncs.com"),
 	new ProductDomain("Slb", "slb.aliyuncs.com"),
-	new ProductDomain("Oss", "oss-cn-hangzhou.aliyuncs.com"),
+	new ProductDomain("Oss", env('OSS_ENDPOINT')),
 	new ProductDomain("OssAdmin", "oss-admin.aliyuncs.com"),
 	new ProductDomain("Sts", "sts.aliyuncs.com"),
 	new ProductDomain("Yundun", "yundun-cn-hangzhou.aliyuncs.com"),
@@ -42,6 +42,6 @@ $productDomains =array(
 	new ProductDomain("Ubsms", "ubsms.aliyuncs.com"),
 	new ProductDomain("Ubsms-inner", "ubsms-inner.aliyuncs.com")
 	);
-$endpoint = new Endpoint("cn-hangzhou", $regionIds, $productDomains);
+$endpoint = new Endpoint(env('STS_REGIN_ID'), $regionIds, $productDomains);
 $endpoints = array($endpoint);
 EndpointProvider::setEndpoints($endpoints);
