69f50e31321db570b6498b0cf2c0e114f10f15b7
wufly <fei.wu@patpat.com>
2018-11-01 16:31:50 +0800

    wf

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,8 +37,14 @@ class Handler extends ExceptionHandler
     public function report(Exception $exception)
     {
         if (env('DING_ENABLED', '') === true) {
-            if ($exception->getMessage() != 'Unauthenticated.') {
-                ding()->text('运营后台：'.$exception->getMessage());
+            if ($exception->getMessage() && $exception->getMessage() != 'Unauthenticated.') {
+                $title = '运营后台错误信息';
+                $markdown = "#### 错误信息  \n ".
+                    "地址：{$exception->getFile()}\n\n ".
+                    "行数：{$exception->getLine()}\n\n".
+                    "用户IP： ".getIP()."\n\n".
+                    "错误信息：".$exception->getMessage();
+                ding()->markdown($title,$markdown);
             }
         }
         parent::report($exception);
