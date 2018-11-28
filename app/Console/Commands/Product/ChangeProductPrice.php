bb31a7c0e1b20b6270d549b0a53e3d4e459c856f
wufly <fei.wu@patpat.com>
2018-11-28 14:27:03 +0800

    批量修改商品价格

--- app/Console/Commands/Product/ChangeProductPrice.php
+++ app/Console/Commands/Product/ChangeProductPrice.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands\Product;
 
+use App\Entities\Good\Good;
 use App\Entities\Good\GoodSku;
 use App\Entities\Product\Product;
+use App\Entities\Product\ProductSku;
 use Illuminate\Console\Command;
 use DB;
 
@@ -52,17 +54,41 @@ class ChangeProductPrice extends Command
 
     public function handleProgress()
     {
-        $category_ids = [798];
+        $category_ids = [1,172,798,508];
         $this->info('start...');
         foreach ($category_ids as $category_id)
         {
             //查找该类目下所有的商品
-            $goods = Product::where('category_path', 'like', '0,'.$category_id.',%')->get();
+            $goods = Good::where('category_path', 'like', '0,'.$category_id.',%')
+                ->where('status', 4)
+                ->get();
             foreach($goods as $good) {
-                DB::table('goods')->where('id', $good->id)->update(['price' => 0]);
-                DB::table('audit_goods')->where('id', $good->id)->update(['price' => 0]);
-                DB::table('good_skus')->where('good_id', $good->id)->update(['price' => 0]);
-                DB::table('audit_good_skus')->where('good_id', $good->id)->update(['price' => 0]);
+                $change = false;
+                //查找所有的sku
+                $skus = GoodSku::where('good_id', $good->id)->get();
+                foreach ($skus as $sku) {
+                    // 判断售价 = 供货价*3/6.95
+                    if ($sku->price < $sku->supply_price*3/6.95) {
+                        $change = true;
+                        $price = round($sku->supply_price*3/6.95*2,0)/2;
+                        $origin_price = $price*1.3;
+                        $sku->price = $price;
+                        // $sku->origin_price = $origin_price;
+                        $sku->save();
+                        $sku_id = $sku->id;
+                        ProductSku::where('id', $sku_id)->update(['origin_price' => $origin_price, 'price' => $price]);
+                    }
+                }
+                if ($change) {
+                    $good_price = $skus->min('price');
+                    // $good_origin_price = $sku->min('origin_price');
+                    // $good->origin_price = $good_origin_price;
+                    $good->price = $good_price;
+                    $good->save();
+                    $origin_price = ProductSku::where('good_id', $good->id)->min('origin_price');
+                    Product::where('id', $good->id)->update(['origin_price' => $origin_price, 'price' => $good_price]);
+                }
+                $this->info($good->id.'finished');
             }
         }
         $this->info('end...');
