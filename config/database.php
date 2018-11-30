e9e67122ea43f5994672bd62296a63722f08992a
wufly <fei.wu@patpat.com>
2018-11-30 14:43:59 +0800

    elasticsearch数据库配置

--- config/database.php
+++ config/database.php
@@ -81,6 +81,11 @@ return [
             'prefix' => '',
         ],
 
+        'elasticsearch' => [
+            // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
+            'hosts' => explode(',', env('ES_HOSTS')),
+        ]
+
     ],
 
     /*
